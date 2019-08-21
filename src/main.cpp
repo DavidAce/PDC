eef146dae6859ffd26531b01e97d49b718a40c91
DavidAce <aceituno@kth.se>
2019-08-21 14:34:23 +0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+ 
+
+#include <omp.h>
+#include <iostream>
+#include <iomanip>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+int main (){
+    auto log = spdlog::stdout_color_mt("OMP");
+    log->set_pattern("[%Y-%m-%d %H:%M:%S][%n]%^[%=8l]%$ %v");
+
+    omp_set_num_threads(8);
+    constexpr long num_steps = 10000000;
+    double time  = omp_get_wtime();
+    double sum = 0.0;
+    double step = 1.0/num_steps;
+    #pragma omp parallel for reduction(+:sum) shared(step) default(none) schedule(static)
+    for (long i = 0; i < num_steps; i++){
+        double x = (i+0.5)*step;
+        sum += 4.0/(1.0+x*x);
+    }
+    log->info("pi = {:.16f}. Time: {:.4f} ms", sum*step, 1000 *(omp_get_wtime()-time));
+    #pragma omp parallel shared(log) default(none)
+    {
+        #pragma omp critical
+        log->info("hello from id {}", omp_get_thread_num());
+
+    }
+
+    return 0;
+
+}
