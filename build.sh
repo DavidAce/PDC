5f325a5240a44ad1d648a26ac6f4aa91a646807a
DavidAce <aceituno@kth.se>
2019-08-22 17:51:31 +0200

    Rebuild into c++

--- build.sh
+++ build.sh
@@ -135,14 +135,14 @@ elif [[ "$HOSTNAME" == *"anderson"* ]];then
     else
         module load OpenBLAS
     fi
-    module load XZ/5.2.4-GCCcore-8.2.0 
-    module load arpack-ng
-    module load ARPACK++
-    module load HDF5/1.10.5-GCCcore-8.2.0
-    module load Eigen
-    module load gflags
-    module load glog
-    module load CMake
+#    module load XZ/5.2.4-GCCcore-8.2.0
+#    module load arpack-ng
+#    module load ARPACK++
+#    module load HDF5/1.10.5-GCCcore-8.2.0
+#    module load Eigen
+#    module load gflags
+#    module load glog
+#    module load CMake
     module load GCCcore
     module list
     if [ "$compiler" = "GNU" ] ; then
