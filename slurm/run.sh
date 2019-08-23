711138419300bfa1719b80cc5df09d362d3ab8c0
DavidAce <aceituno@kth.se>
2019-08-23 14:30:14 +0200

    Added slurm batch script and submit file

---
+++ slurm/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --kill-on-invalid-dep=yes
+#SBATCH --output=logs/cuda.out
+#SBATCH --error=logs/cuda.err
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH -C Haswell
+#SBATCH --gres=gpu:K420:1
+#SBATCH --time=00:01:00 
+#SBATCH -A edu19.summer
+#SBATCH --reservation=summer-2019-08-23
+
+srun ../build/lab1
