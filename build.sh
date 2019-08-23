8729e54a565f1dc8968e880cc3196c5b47bb8ec3
DavidAce <aceituno@kth.se>
2019-08-23 14:53:51 +0200

    Added build script

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 module load cuda/7.0
-
+module load cmake/3.11.4
+module load clang/7.0.0
 cmake -E make_directory build
 cd build
 cmake -DCMAKE_BUILD_TYPE=$build  -G "CodeBlocks - Unix Makefiles" ../
