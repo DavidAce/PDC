6a63b1b04bbea20dcd863ddfb23274d84badb1ab
DavidAce <aceituno@kth.se>
2019-08-26 16:46:08 +0200

    Started lab 3

--- src/lab02_ex3_6.cu
+++ src/lab02_ex3_6.cu
@@ -327,6 +327,30 @@ __global__ void gpu_sobel(int width, int height, float *image, float *image_out)
     // TO-DO #6.1 /////////////////////////////////////
     // Implement the GPU version of the Sobel filter //
     ///////////////////////////////////////////////////
+    float sobel_x[9] = { 1.0f,  0.0f, -1.0f,
+                         2.0f,  0.0f, -2.0f,
+                         1.0f,  0.0f, -1.0f };
+    float sobel_y[9] = { 1.0f,  2.0f,  1.0f,
+                         0.0f,  0.0f,  0.0f,
+                        -1.0f, -2.0f, -1.0f };
+    
+    int index_x = blockIdx.x * blockDim.x + threadIdx.x;
+    int index_y = blockIdx.y * blockDim.y + threadIdx.y;
+    
+    if (index_x < (width - 2) && index_y < (height - 2))
+    {
+        int offset_t = index_y * width + index_x;
+        int offset   = (index_y + 1) * width + (index_x + 1);
+        
+        float gx = gpu_applyFilter(&image[offset_t], width, sobel_x, 3);
+        float gy = gpu_applyFilter(&image[offset_t], width, sobel_y, 3);
+        
+        // Note: The output can be negative or exceed the max. color value
+        // of 255. We compensate this afterwards while storing the file.
+        image_out[offset] = sqrtf(gx * gx + gy * gy);
+
+    }
+    
 }
 
 int main(int argc, char **argv)
@@ -433,11 +457,11 @@ int main(int argc, char **argv)
         store_result("cpu_3", elapsed[0], elapsed[1], bitmap.width, bitmap.height, image_out[0]);
         // Launch the GPU version
         gettimeofday(&t[0], NULL);
-        // gpu_sobel<<<grid, block>>>(bitmap.width, bitmap.height,
-        //                            d_image_out[1], d_image_out[0]);
+        gpu_sobel<<<grid, block>>>(bitmap.width, bitmap.height,
+                                   d_image_out[1], d_image_out[0]);
         
-        // cudaMemcpy(image_out[0], d_image_out[0],
-        //            image_size * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(image_out[0], d_image_out[0],
+                   image_size * sizeof(float), cudaMemcpyDeviceToHost);
         gettimeofday(&t[1], NULL);
         
         elapsed[1] = get_elapsed(t[0], t[1]);
