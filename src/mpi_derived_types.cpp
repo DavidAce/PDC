baf9133c36f370cdd7eb5e80f1c934e8274f33d0
DavidAce <aceituno@kth.se>
2019-08-28 16:13:07 +0200

    Almost finished ex2

---
+++ src/mpi_derived_types.cpp
@@ -0,0 +1,105 @@
+//
+// Created by david on 2019-08-28.
+//
+
+
+#include <string>
+#include <mpi.h>
+#include <fstream>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <vector>
+
+constexpr int STL_HDR_SIZE = 80;
+
+typedef struct {
+    float n[3];			/* Normal vector */
+    float v1[3];			/* Vertex 1 */
+    float v2[3];			/* Vertex 2 */
+    float v3[3];			/* Vertex 3 */
+    uint16_t attrib;		/* Attribute byte count */
+} __attribute__((packed))
+        stl_triangle_cpp;
+
+
+typedef struct {
+    char hdr[STL_HDR_SIZE];	/* Header */
+    uint32_t n_tri;		/* Number of triangles */
+    std::vector<stl_triangle_cpp> tri;
+} stl_model_cpp;
+
+
+
+void stl_read(const std::string &fname, stl_model_cpp &model) {
+    int pe_size, pe_rank;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &pe_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &pe_rank);
+
+
+    std::ifstream infile (fname, std::ifstream::binary);
+    if (not infile.good()) throw std::runtime_error("File not found");
+    /* Read STL header */
+    infile.read(model.hdr,STL_HDR_SIZE*sizeof(char));
+
+    if (pe_rank == 0) std::cout << "Reading STL file: " << fname << std::endl;
+    /* Make sure it's a binary STL file */
+    if (strncmp(model.hdr, "solid", 5) == 0) {
+        fprintf(stderr, "ASCII STL files not supported!\n");
+        exit(-1);
+    }
+
+    /* Read how many triangles the file contains */
+    infile.read((char*)&model.n_tri,1*sizeof(uint32_t));
+    if (pe_rank == 0) printf("Found: %d triangles\n", model.n_tri);
+
+    /* Allocate memory for triangles, and read them */
+    model.tri.resize(model.n_tri);
+    infile.read((char*)model.tri.data(), model.n_tri * sizeof(stl_triangle_cpp) );
+    infile.close();
+    if (pe_rank == 0) printf("Done\n");
+
+}
+
+void stl_write(const std::string &fname, stl_model_cpp &model) {
+    int pe_size, pe_rank;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &pe_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &pe_rank);
+    std::ofstream outfile (fname, std::ifstream::binary);
+    if (pe_rank == 0) std::cout << "Writing STL file: " << fname << std::endl;
+
+    /* Write STL header */
+    outfile.write(model.hdr,STL_HDR_SIZE*sizeof(char));
+
+
+    /* Write number of triangles */
+    outfile.write((char*)&model.n_tri,1*sizeof(uint32_t));
+    if (pe_rank == 0) printf("Wrote: %d triangles\n", model.n_tri);
+
+
+
+    /* Write all triangles */
+    outfile.write((char*)model.tri.data(), model.n_tri * sizeof(stl_triangle_cpp) );
+    outfile.close();
+    if (pe_rank == 0) printf("Done\n");
+}
+
+
+int main(int argc, char **argv) {
+    stl_model_cpp model;
+
+    MPI_Init(&argc, &argv);
+
+    stl_read("data/sphere.stl", model);
+    stl_write("data/out.stl", model);
+
+    MPI_Finalize();
+
+    return 0;
+}
+
+
---
+++ src/mpi_derived_types_parallel.cpp
@@ -0,0 +1,139 @@
+//
+// Created by david on 2019-08-28.
+//
+
+//
+// Created by david on 2019-08-28.
+//
+
+
+#include <string>
+#include <mpi.h>
+#include <fstream>
+#include <vector>
+
+constexpr int STL_HDR_SIZE = 80;
+
+typedef struct {
+    float n[3];			/* Normal vector */
+    float v1[3];			/* Vertex 1 */
+    float v2[3];			/* Vertex 2 */
+    float v3[3];			/* Vertex 3 */
+    uint16_t attrib;		/* Attribute byte count */
+} __attribute__((packed))
+        stl_triangle_cpp;
+MPI_Datatype stl_triangle_mpi_struct;
+MPI_Datatype stl_triangle_mpi_packed;
+
+
+typedef struct {
+    char hdr[STL_HDR_SIZE];	/* Header */
+    uint32_t n_tri;		/* Number of triangles */
+    std::vector<stl_triangle_cpp> tri;
+} stl_model_cpp;
+
+
+
+void stl_read(const std::string &fname, stl_model_cpp &model) {
+    int pe_size, pe_rank;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &pe_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &pe_rank);
+
+    MPI_File infile;
+    int err = MPI_File_open(MPI_COMM_WORLD,fname.c_str(),MPI_MODE_RDONLY, MPI_INFO_NULL,&infile );
+    if (err < 0) throw std::runtime_error("File not found");
+    /* Read STL header */
+    MPI_File_read_all(infile, model.hdr, STL_HDR_SIZE, MPI_CHAR, MPI_STATUS_IGNORE);
+
+    if (pe_rank == 0) std::cout << "Reading STL file: " << fname << std::endl;
+    /* Make sure it's a binary STL file */
+    if (strncmp(model.hdr, "solid", 5) == 0) {
+        fprintf(stderr, "ASCII STL files not supported!\n");
+        exit(-1);
+    }
+    MPI_File_read_all(infile, &model.n_tri, 1, MPI_UINT32_T, MPI_STATUS_IGNORE);
+    if (pe_rank == 0) printf("Found: %d triangles\n", model.n_tri);
+
+    int nlocal =  (model.n_tri + pe_size - pe_rank - 1) / pe_size;
+    int offset = 0;
+    MPI_Exscan(&nlocal, &offset,  1, MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+    std::cout << "Read ID: " << pe_rank << " nlocal: " << nlocal << " offset: " << offset << " + " << std::to_string(STL_HDR_SIZE) << std::endl;
+    /* Allocate memory for triangles, and read them */
+    model.tri.resize(nlocal);
+    offset += STL_HDR_SIZE;
+    MPI_File_read_at_all(infile, offset, model.tri.data(), nlocal, stl_triangle_mpi_packed, MPI_STATUS_IGNORE );
+    MPI_File_close(&infile);
+    if (pe_rank == 0) printf("Done\n");
+
+}
+
+void stl_write(const std::string &fname, stl_model_cpp &model) {
+    int pe_size, pe_rank;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &pe_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &pe_rank);
+    MPI_File outfile;
+    int err = MPI_File_open(MPI_COMM_WORLD,fname.c_str(),MPI_MODE_CREATE + MPI_MODE_WRONLY, MPI_INFO_NULL,&outfile );
+    if (err < 0) throw std::runtime_error("File not found");
+    if (pe_rank == 0) std::cout << "Writing STL file: " << fname << std::endl;
+
+    /* Write STL header */
+    MPI_File_write_all(outfile, model.hdr, STL_HDR_SIZE, MPI_CHAR,MPI_STATUS_IGNORE);
+
+    /* Write number of triangles */
+    MPI_File_write_all(outfile, &model.n_tri,1,MPI_UINT32_T, MPI_STATUS_IGNORE);
+    if (pe_rank == 0) printf("Writing: %d triangles\n", model.n_tri);
+
+
+    /* Write all triangles */
+    int nlocal = model.tri.size();
+    int offset = 0;
+    int ntotal;
+    MPI_Exscan(&nlocal, &offset, 1, MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Reduce(&nlocal, &ntotal, 1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+    std::cout << "Write ID: " << pe_rank << " nlocal: " << nlocal << " offset: "  << " + " << std::to_string(STL_HDR_SIZE)<< offset << std::endl;
+    offset += STL_HDR_SIZE;
+    MPI_File_write_at_all(outfile, offset, model.tri.data(), nlocal, stl_triangle_mpi_packed, MPI_STATUS_IGNORE);
+    MPI_File_close(&outfile);
+    if (pe_rank == 0) printf("Wrote: %d triangles\n", ntotal);
+    if (pe_rank == 0) printf("Done\n");
+}
+
+
+int main(int argc, char **argv) {
+    stl_model_cpp model;
+
+    MPI_Init(&argc, &argv);
+
+    int len[5] = {3,3,3,3,1};
+    MPI_Datatype types[5] = {MPI_FLOAT,MPI_FLOAT,MPI_FLOAT,MPI_FLOAT,MPI_UINT16_T};
+    MPI_Aint base,displ[5], sizeofstruct;
+    stl_triangle_cpp dummy;
+    MPI_Get_address(&dummy.n     , displ + 0);
+    MPI_Get_address(&dummy.v1    , displ + 1);
+    MPI_Get_address(&dummy.v2    , displ + 2);
+    MPI_Get_address(&dummy.v3    , displ + 3);
+    MPI_Get_address(&dummy.attrib, displ + 4);
+    base = displ[0];
+    for (int i = 0; i < 5; i++) MPI_Aint_diff(displ[i],base);
+    MPI_Type_create_struct(5,len, displ,types,&stl_triangle_mpi_struct);
+
+    MPI_Get_address(&dummy + 1 , &sizeofstruct);
+    sizeofstruct = MPI_Aint_diff(sizeofstruct,base);
+    MPI_Type_create_resized(stl_triangle_mpi_struct, 0, sizeofstruct, &stl_triangle_mpi_packed);
+    MPI_Type_commit(&stl_triangle_mpi_packed);
+
+
+
+
+    stl_read("data/sphere.stl", model);
+    stl_write("data/out.stl", model);
+
+    MPI_Finalize();
+
+    return 0;
+}
+
+
