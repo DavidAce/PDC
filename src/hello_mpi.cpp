6c56ae16e985a307d8f61e132b45761c068e6a9f
DavidAce <aceituno@kth.se>
2019-08-28 13:47:59 +0200

    Finished ex3

--- src/hello_mpi.cpp
+++ src/hello_mpi.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <mpi.h>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
 
 int main (int argc, char *argv[])
 {
@@ -10,9 +13,15 @@ int main (int argc, char *argv[])
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   MPI_File hellofile;
-  MPI_File_open(MPI_COMM_WORLD,"data/helloworld.txt", MPI_MODE_WRONLY, MPI_INFO_NULL,&hellofile );
+  MPI_File_open(MPI_COMM_WORLD,"data/helloworld.txt",MPI_MODE_WRONLY+ MPI_MODE_CREATE, MPI_INFO_NULL,&hellofile );
 
-  std::string hello = "Processor " + std::to_string(myrank) + " of " + std::to_string(size) + ": Hello World!\n";
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+
+    std::ostringstream oss;
+    oss << std::put_time(&tm, "%d-%m-%Y %H:%M:%S");
+
+  std::string hello = "Processor " + std::to_string(myrank) + " of " + std::to_string(size) + ": Hello World! Time: " + oss.str() + '\n';
 
   int offset = myrank * hello.size();
   int count = hello.size();
