0af0364e6058de286a41ce0d9a935db4d79ef780
DavidAce <aceituno@kth.se>
2019-08-29 10:36:23 +0200

    Debugging ex2

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #/bin/bash
-#module purge
-module load PrgEnv-cray/6.0.5
-module load gcc/6.2.0
-module load cray-mpich
-module load cmake/3.11.4
+module purge
+module load PrgEnv-gnu/6.0.5
+module load cmake/3.14.5
 
-export CC=gcc
-export CXX=g++
+export CC=cc
+export CXX=CC
 cmake -E make_directory build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release  -G "CodeBlocks - Unix Makefiles" ../
